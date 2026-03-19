85c1bf9ac0b56a5c0704d15c25f1ad1f6c76c9b1
Ben Lufuta <ben.lufuta@gmail.com>
2026-03-19 13:10:07 -0400

    chore: add unit testing classes and methods for Application and ApplicationService classes.

---
+++ src/test/java/com/benlufuta/hirelog/domain/ApplicationTest.java
@@ -0,0 +1,48 @@
+package com.benlufuta.hirelog.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    void testAssignId() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testGetId() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testGetStatus() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testMarkAsApplied() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testMarkAsInterviewing() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testMarkAsRejected() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testSetNextFollowUpDate() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testUpdateNotes() {
+        fail("This test method is yet to be implemented!");
+    }
+}
---
+++ src/test/java/com/benlufuta/hirelog/service/ApplicationServiceTest.java
@@ -0,0 +1,35 @@
+package com.benlufuta.hirelog.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationServiceTest {
+
+    @Test
+    void testAddApplication() {
+        fail("This test method is yet to be implemented!");
+        assertEquals(5, 5,0);
+
+    }
+
+    @Test
+    void testDeleteApplication() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testFindById() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testFindByStatus() {
+        fail("This test method is yet to be implemented!");
+    }
+
+    @Test
+    void testGetAllApplications() {
+        fail("This test method is yet to be implemented!");
+    }
+}
