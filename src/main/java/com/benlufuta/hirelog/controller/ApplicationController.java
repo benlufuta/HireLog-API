93588b760faa76c4b7fff3f841874a01503bedc9
Ben Lufuta <ben.lufuta@gmail.com>
2026-03-27 23:04:26 -0400

    fix(api): change controller method for cleaner naming

--- src/main/java/com/benlufuta/hirelog/controller/ApplicationController.java
+++ src/main/java/com/benlufuta/hirelog/controller/ApplicationController.java
@@ -1,10 +1,13 @@
 package com.benlufuta.hirelog.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.benlufuta.hirelog.domain.Application;
 import com.benlufuta.hirelog.service.ApplicationService;
 
 @RestController
@@ -19,13 +22,13 @@ public class ApplicationController {
     }
 
     @GetMapping
-    public Object getAllApplications(){
+    public List<Application> getAllApplications(){
 
         return service.getAllApplications();
     }
 
     @GetMapping("/{id}")
-    public Object findById(@PathVariable ("id") Long id){
+    public Application getApplicationById(@PathVariable ("id") Long id){
 
         return service.findById(id);
     }
